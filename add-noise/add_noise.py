5b30a18233e1ff0ae082d47a26f4bb0a097ec6b1
Resistence <1760921465@qq.com>
2018-08-14 22:46:18 +0800

    图像上增加高斯白噪声

--- add-noise/add_noise.py
+++ add-noise/add_noise.py
@@ -6,12 +6,13 @@ import os
 def addGausianNosie(src,means,sigma):
     noise = np.random.normal(means,sigma,size=src.shape)
     GaussianImg = src + noise
-    return GaussianImg
+    GaussianImg_clip = np.clip(GaussianImg,0,255)
+    return GaussianImg_clip
 
-dir = 'Gray/'
+dir = '/root/hj9/images/Gray_train/'
 files = os.listdir(dir)
 for file in files:
     img = cv2.imread(dir+file)
-    for index in range(10):
+    for index in range(1700):
         res = addGausianNosie(img,0,10)
-        cv2.imwrite('Gaussian/'+file[0]+'_'+str(index)+'.png',res)
+        cv2.imwrite('/root/hj9/images/train/'+file[0:-4]+'_'+str(index)+'.jpg',res)
