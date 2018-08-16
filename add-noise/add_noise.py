74e80368846a8d5dd929b595e9b2ea2168008b3d
Resistence <1760921465@qq.com>
2018-08-16 17:11:26 +0800

    Update add_noise.py

--- add-noise/add_noise.py
+++ add-noise/add_noise.py
@@ -1,18 +1,24 @@
-
 import cv2
 import numpy as np
 import os
+from PIL import Image
 
 def addGausianNosie(src,means,sigma):
-    noise = np.random.normal(means,sigma,size=src.shape)
+    src = np.array(src)
+    x = src.shape[0]
+    y = src.shape[1]
+    noise = means+sigma * np.random.randn(x,y)
     GaussianImg = src + noise
     GaussianImg_clip = np.clip(GaussianImg,0,255)
     return GaussianImg_clip
 
-dir = '/root/hj9/images/Gray_train/'
+dir = '/root/hj9/images2/train_gt/'
 files = os.listdir(dir)
 for file in files:
-    img = cv2.imread(dir+file)
+    img = Image.open(dir+file)
     for index in range(1700):
         res = addGausianNosie(img,0,10)
-        cv2.imwrite('/root/hj9/images/train/'+file[0:-4]+'_'+str(index)+'.jpg',res)
+        res = Image.fromarray(res)
+        res = res.convert('L')
+        # res.save('/root/hj9/images2/val_noise/'+file)
+        res.save('/root/hj9/images2/train_noise/' + file[0:-4]+'_'+str(index)+'.jpg')
